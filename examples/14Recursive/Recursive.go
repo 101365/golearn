2be1aa75e19f2b7ae7cfeae7e5538f9362800396
1010365 <nextflag@163.com>
2018-02-26 11:46:46 +0800

    新增递归演示代码

--- examples/14Recursive/Recursive.go
+++ examples/14Recursive/Recursive.go
@@ -1,10 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // 演示递归 和其他语言一致 这里演示阶乘的实现
 
 func factorial(v int) int  {
+	if v < 1 {
+		panic("参数不能小于0")
+	}
 	if v == 1 {
 		return 1
 	}
@@ -15,5 +20,7 @@ func main() {
 
 	total := factorial(5)
 	fmt.Println(total)
+
+	factorial(-2)
 
 }
