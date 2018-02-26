42bbe948f7fed26ecbdda9c7fec1851192353cb8
1010365 <nextflag@163.com>
2018-02-26 10:52:35 +0800

    函数多返回值

--- examples/12func/Func.go
+++ examples/12func/Func.go
@@ -8,10 +8,19 @@ func add(x, y int) int {
 	return x + y
 }
 
+// 多返回值
+func demo() (int, int)  {
+	return 2, 8
+}
+
 func main() {
 
 	sum := add(2, 8)
 
 	fmt.Println("2 + 8 = ", sum)
+
+	// 多返回值
+	x, y := demo()
+	fmt.Println(x, ", ", y)
 
 }
