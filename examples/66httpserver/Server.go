e6dd4938615d6e1861d2ae318fde38d79765ea30
1010365 <nextflag@163.com>
2018-02-26 16:24:31 +0800

    新增http服务器演示代码

---
+++ examples/66httpserver/Server.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"strings"
+	"log"
+)
+
+func sayHello(w http.ResponseWriter, r *http.Request)  {
+	r.ParseForm()
+	fmt.Println(r.Form)
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("value:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "Hello world!!!")
+}
+
+// 启动运行  在浏览器访问 http://localhost:9090 即可显示：Hello world!!!
+func main() {
+
+	http.HandleFunc("/", sayHello)
+
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+
+}
