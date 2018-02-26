0a463cac4758a438bc645f14e56df624072540f2
1010365 <nextflag@163.com>
2018-02-26 11:06:37 +0800

    新增可变参数列表示例代码

--- examples/12func/Func.go
+++ examples/12func/Func.go
@@ -13,6 +13,17 @@ func demo() (int, int)  {
 	return 2, 8
 }
 
+// 可变参数函数  类似于Java中的可变参数列表
+func multi(nums ...int) int  {
+	fmt.Println(nums, " ")
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+	fmt.Println("求和：", total)
+	return total
+}
+
 func main() {
 
 	sum := add(2, 8)
@@ -26,5 +37,14 @@ func main() {
 	// 忽略部分返回值
 	_, k := demo()
 	fmt.Println(k)
+
+	// 可变参数列表
+	multi(1,2)
+	multi(1)
+	multi(1, 2, 3, 4, 5)
+
+	// 切片作为参数传入 需要解包
+	nums := []int{1,2,3}
+	multi(nums...)
 
 }
