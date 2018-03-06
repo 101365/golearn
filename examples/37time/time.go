fd985b769e8aa9552af5587f453c0682723ca47f
1010365 <nextflag@163.com>
2018-03-06 19:39:56 +0800

    添加时间戳、时间格式化示例代码

--- examples/37time/time.go
+++ examples/37time/time.go
@@ -5,11 +5,70 @@ import (
 	"time"
 )
 
-// 演示time
+// 演示time 时间 以及时间戳
 var p = fmt.Println
 
 func main() {
 
+	// 演示时间
+	showTime()
+
+	// 演示时间戳
+	showTimestamp()
+
+	// 演示时间格式化
+	formatTimeDemo()
+
+}
+
+// 演示时间格式化
+func formatTimeDemo() {
+
+	t := time.Now()
+	p(t.Format(time.RFC3339))
+
+	t1, e := time.Parse(
+		time.RFC3339,
+		"2012-11-01T22:08:41+00:00")
+	p(t1)
+
+	p(t.Format("3:04PM"))
+	p(t.Format("Mon Jan _2 15:04:05 2006"))
+	p(t.Format("2006-01-02T15:04:05.999999-07:00"))
+	form := "3 04 PM"
+	t2, e := time.Parse(form, "8 41 PM")
+	p(t2)
+
+	fmt.Printf("%d-%02d-%02dT%02d:%02d:%02d-00:00\n",
+		t.Year(), t.Month(), t.Day(),
+		t.Hour(), t.Minute(), t.Second())
+
+	ansic := "Mon Jan _2 15:04:05 2006"
+	_, e = time.Parse(ansic, "8:41PM")
+	p(e)
+
+}
+
+// 演示时间戳
+func showTimestamp() {
+
+	now := time.Now()
+	secs := now.Unix()
+	nanos := now.UnixNano()
+	fmt.Println(now)
+
+	millis := nanos / 1000000
+	fmt.Println(secs)
+	fmt.Println(millis)
+	fmt.Println(nanos)
+
+	fmt.Println(time.Unix(secs, 0))
+	fmt.Println(time.Unix(0, nanos))
+
+}
+
+// 演示时间
+func showTime() {
 	// 当前时间
 	now := time.Now()
 	p(now)
@@ -46,5 +105,4 @@ func main() {
 	// 时间加减
 	p(then.Add(diff))
 	p(then.Add(-diff))
-
 }
