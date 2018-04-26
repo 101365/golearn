01781e71dff76e635bc5d381b011b0296b13c3f1
1010365 <nextflag@163.com>
2018-04-26 17:34:14 +0800

    small kit

--- validate/01assign/copy_test.go
+++ validate/01assign/copy_test.go
@@ -9,4 +9,8 @@ func TestCopy(t *testing.T) {
 	bytes := []byte("hello world")
 	copy(bytes,"ha ha")
 	fmt.Println(string(bytes))
+
+	var asciiSpace = [256]uint8{'\t': 1, '\n': 1, '\v': 1, '\f': 1, '\r': 1, ' ': 1, 100: 1}
+	fmt.Println(asciiSpace)
+	fmt.Println(int('\t'))
 }
