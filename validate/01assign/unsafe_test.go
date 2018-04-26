36106820470c4bac3b8bd80f6d71c4ddd4217781
1010365 <nextflag@163.com>
2018-04-26 13:50:01 +0800

    modify unsafe test code

--- validate/01assign/unsafe_test.go
+++ validate/01assign/unsafe_test.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 )
 
-func TestUnsafe(t *testing.T) {
+func TestUnsafePointer(t *testing.T) {
 
 	s := struct {
 		a byte
@@ -66,5 +66,37 @@ func TestUnsafe(t *testing.T) {
 	*pi = 40
 	// 结构体内容跟着改变
 	fmt.Println(s)
+	fmt.Println(unsafe.Alignof(s.d))
+	fmt.Println(p)
+	fmt.Println(&s.d)
+
+}
+
+func TestAlign(t *testing.T) {
+
+	fmt.Println("|", "类型", "|", "对齐值")
+	fmt.Println("|", "byte", "|", unsafe.Alignof(byte(0)))
+	fmt.Println("|", "int8", "|", unsafe.Alignof(int8(0)))
+	fmt.Println("|", "unit8", "|", unsafe.Alignof(uint8(0)))
+	fmt.Println("|", "int16", "|", unsafe.Alignof(int16(0)))
+	fmt.Println("|", "uint16", "|", unsafe.Alignof(uint16(0)))
+	fmt.Println("|", "int32", "|", unsafe.Alignof(int32(0)))
+	fmt.Println("|", "uint32", "|", unsafe.Alignof(uint32(0)))
+	fmt.Println("|", "int64", "|", unsafe.Alignof(int64(0)))
+	fmt.Println("|", "uint64", "|", unsafe.Alignof(uint64(0)))
+	fmt.Println("|", "unitptr", "|", unsafe.Alignof(uintptr(0)))
+	fmt.Println("|", "float32", "|", unsafe.Alignof(float32(0)))
+	fmt.Println("|", "float64", "|", unsafe.Alignof(float64(0)))
+	fmt.Println("|", "complex64", "|", unsafe.Alignof(complex64(0)))
+	fmt.Println("|", "complex64", "|", unsafe.Alignof(complex64(0)))
+	fmt.Println("|", "string", "|", unsafe.Alignof(""))
+	fmt.Println("|", "int", "|", unsafe.Alignof(new(int)))
+	fmt.Println("|", "struct", "|", unsafe.Alignof(struct {
+		f  float32
+		ff float64
+	}{}))
+	fmt.Println("|", "chan", "|", unsafe.Alignof(make(chan bool, 10)))
+	fmt.Println("|", "[]int", "|", unsafe.Alignof(make([]int, 10)))
+	fmt.Println("|", "map", "|", unsafe.Alignof(make(map[string]string, 10)))
 
 }
