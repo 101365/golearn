a26e8ebc676b79388e662b48e0dfbc33340e0a00
101365 <nextflag@163.com>
2019-02-12 17:53:52 +0800

    update flag demo

--- demo/01flag/flag.go
+++ demo/01flag/flag.go
@@ -8,10 +8,11 @@ import (
 var version string
 
 func init() {
-	flag.StringVar(&version, "version", "def_version", "")
+	flag.StringVar(&version, "version", "def_version", "test")
 }
 
 // go run flag.go -version=123
+// go run flag.go --help
 func main() {
 	flag.Parse()
 	fmt.Println("version => ", version)
