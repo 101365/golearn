e7e152f52e878677cec201bce6549611f26123a8
101365 <nextflag@163.com>
2019-03-18 10:10:41 +0800

    模拟请求外部地址

--- demo/06/convert.go
+++ demo/06/convert.go
@@ -2,6 +2,27 @@ package main
 
 import "fmt"
 
+type MyString = string
+type MyString2 string
+type MyString3 = MyString2
+
+func (MyString3) t() {
+
+}
+
 func main() {
 	fmt.Println(string(-1))
+
+	var s1 MyString2
+	s1 = "abc"
+	switch interface{}(s1).(type) {
+	case string:
+		fmt.Println("string")
+	case MyString2:
+		fmt.Println("MyString2")
+	}
+
+	ok := string(s1)
+	fmt.Println(ok)
+
 }
